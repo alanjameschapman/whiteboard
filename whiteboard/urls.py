706109b4f834ae7baed9bf0c611b678758909e51
Alan <alanjameschapman@gmail.com>
2024-02-28 20:58:32 +0000

    Add edblog app and its files

--- whiteboard/urls.py
+++ whiteboard/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from hello_world import views as index_views
+from edblog import views as index_views
 
 urlpatterns = [
-    path('hello/', index_views.home),
+    path('edblog/', index_views.home),
     path('admin/', admin.site.urls),
 ]
