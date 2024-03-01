aa5bcda7c1989ec8b2a1987afbc4a151c4e994e2
Alan <alanjameschapman@gmail.com>
2024-03-01 20:47:38 +0000

    Add Comment model and update Post model

--- edblog/models.py
+++ edblog/models.py
@@ -5,9 +5,27 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.CharField(max_length=200, blank=True)
+
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`edblog.Post`.
+    """
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
