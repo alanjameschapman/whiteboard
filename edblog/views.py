4ab62ec206a761c03f815bb90fcd95a04272ae64
Alan <alanjameschapman@gmail.com>
2024-03-03 21:08:47 +0000

    Install crispy forms and crispy bootstrap, update post_detail view, and update template to include styled form.

--- edblog/views.py
+++ edblog/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
+
 
 # Create your views here.
+
+
 class PostList(generic.ListView):
     """
     Display a list of :model:`blog.Post` objects.
     """
-    queryset = Post.objects.filter(status=1).order_by('-created_on') # pylint: disable=no-member
+    queryset = Post.objects.filter(status=1).order_by('-created_on')  # pylint: disable=no-member
     # tailored to show latest and published posts first
     template_name = "edblog/index.html"
-    paginate_by = 9 # Increased from 6 to 9. Infinite scroll discounted
+    paginate_by = 9  # Increased from 6 to 9. Infinite scroll discounted
     # because it tends to be used for aimless browsing - our users will be
     # looking for specific content. Last 9 posts should be enough to show, but
     # not too many to overwhelm the user.
@@ -30,11 +35,33 @@ def post_detail(request, slug):
     :template:`edblog/post_detail.html`
     """
 
-    queryset = Post.objects.filter(status=1) # pylint: disable=no-member
+    queryset = Post.objects.filter(status=1)  # pylint: disable=no-member
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+            request, messages.SUCCESS,
+            'Comment submitted and awaiting approval'
+    )
+
+
+    comment_form = CommentForm()
 
     return render(
         request,
         "edblog/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
     )
