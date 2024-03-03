c36a35f06669c14db7e43de777939a9fc002de0f
Alan <alanjameschapman@gmail.com>
2024-03-03 10:05:21 +0000

    Add pagination button and enable serving of static files

--- edblog/views.py
+++ edblog/views.py
@@ -4,6 +4,10 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on') # tailored to show latest and published posts first
+    queryset = Post.objects.filter(status=1).order_by('-created_on') # tailored
+    # to show latest and published posts first
     template_name = "edblog/index.html"
-    paginate_by = 9 # Increased from 6 to 9. Consider tailoring for infinite scroll
+    paginate_by = 9 # Increased from 6 to 9. Infinite scroll discounted
+    # because it tends to be used for aimless browsing - our users will be
+    # looking for specific content. Last 9 posts should be enough to show, but
+    # not too many to overwhelm the user.
