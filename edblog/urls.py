15f03e9cc4505c29d2ca90fe834c0c7a4a4e2d8a
Alan <alanjameschapman@gmail.com>
2024-03-03 15:05:09 +0000

    Add boilerplate styling for login and logout templates for use with Allauth

--- edblog/urls.py
+++ edblog/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('', views.PostList.as_view(), name='home'), # as_view() required for class-based views.
     path('<slug:slug>/', views.post_detail, name='post_detail'), # pylint: disable=no-member
 ]
--- whiteboard/urls.py
+++ whiteboard/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("edblog.urls"), name="edblog-urls"),
-    path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("edblog.urls"), name="edblog-urls"),
 ]
