8d8162765f5e38c450d0af2dcdd16512fe6c8246
Alan <alanjameschapman@gmail.com>
2024-03-03 09:10:25 +0000

    Add base and index.html templates, paginate, add boilerplate css.

--- edblog/views.py
+++ edblog/views.py
@@ -4,5 +4,6 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1).order_by('-created_on') # tailored to show latest and published posts first
+    template_name = "edblog/index.html"
+    paginate_by = 9 # Increased from 6 to 9. Consider tailoring for infinite scroll
