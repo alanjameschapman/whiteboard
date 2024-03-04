12331709efe572a9f6f9f0aaffaaf00324201f23
Alan <alanjameschapman@gmail.com>
2024-03-04 14:56:44 +0000

    Add edit and delete functionality with modal confirmation

--- edblog/forms.py
+++ edblog/forms.py
@@ -1,5 +1,8 @@
-from .models import Comment
+"""
+This file is used to create a form for the comment section of the blog.
+"""
 from django import forms
+from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
--- edblog/urls.py
+++ edblog/urls.py
@@ -1,7 +1,11 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'), # as_view() required for class-based views.
     path('<slug:slug>/', views.post_detail, name='post_detail'), # pylint: disable=no-member
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
